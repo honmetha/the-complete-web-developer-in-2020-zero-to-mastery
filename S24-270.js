36eb313f414aa387fd59ec4536b51731131bc5c9
Hon <dennis.mejen@gmail.com>
2020-11-23 21:48:55 +0700

    [Added] Add S24-270

---
+++ S24-270.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  console.log('headers', request.headers);
+  console.log('method', request.method);
+  console.log('url', request.url);
+
+  const user = {
+    name: 'John',
+    hobby: 'Skating'
+  };
+
+  response.setHeader('content-type', 'application/json');
+  response.end(JSON.stringify(user));
+});
+
+server.listen(3000);
