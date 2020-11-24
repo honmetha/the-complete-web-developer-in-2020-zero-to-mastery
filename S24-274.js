5f772b60e4a4d2b47ff174c33c310c3884631a85
Hon <dennis.mejen@gmail.com>
2020-11-24 22:18:31 +0700

    [Added] Add S24-274 Postman

---
+++ S24-274.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('getting root');
+});
+
+app.get('/profile', (req, res) => {
+  res.send('getting profile');
+});
+
+app.post('/profile', (req, res) => {
+  console.log(req.body);
+  res.send('Success');
+});
+
+app.listen(3000);
