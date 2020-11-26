47774b8060cd65d0b359c3f377aeae3b90824cf5
Hon <dennis.mejen@gmail.com>
2020-11-26 23:28:22 +0700

    [Added] Add S24-277 Node File System Module

---
+++ Section 24: Node.js + Express.js/S24-277/script.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+fs.readFile('./hello.txt', (err, data) => {
+  if (err) console.log('error');
+  console.log('Async', data.toString());
+});
+
+const file = fs.readFileSync('./hello.txt');
+console.log('Sync', file.toString());
+
+// APPEND
+fs.appendFile('./hello.txt', ' This is so cool!', err => {
+  if (err) console.log(err);
+});
+
+// WRITE
+fs.writeFile('bye.txt', 'Sad to see you go', err => {
+  if (err) console.log(err);
+});
+
+// DELETE
+fs.unlink('./bye.txt', err => {
+  if (err) console.log(err);
+});
